6b56c9161d985b536b496772d211520e658414d1
Jhonatec <jhonsreis@gmail.com>
2023-10-31 00:16:14 -0300

    fix: types on student.routes

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import StudentController from "../controllers/student.controller";
 import validateCreateStudent from "../middlewares/student.middleware";
 import validateToken from "../middlewares/token.middleware";
@@ -7,23 +7,25 @@ const studentController = new StudentController();
 
 const loginRoutes = Router();
 
-loginRoutes.get("/", validateToken, (req, res) =>
+loginRoutes.get("/", validateToken, (req: Request, res: Response) =>
   studentController.find(req, res)
 );
 
-loginRoutes.post("/", validateCreateStudent, (req, res) =>
+loginRoutes.post("/", validateCreateStudent, (req: Request, res: Response) =>
   studentController.create(req, res)
 );
 
-loginRoutes.put("/", validateToken, (req, res) =>
-  studentController.update(req, res)
+loginRoutes.put(
+  "/",
+  [validateToken, validateCreateStudent],
+  (req: Request, res: Response) => studentController.update(req, res)
 );
 
-loginRoutes.get("/:id", validateToken, (req, res) =>
+loginRoutes.get("/:id", validateToken, (req: Request, res: Response) =>
   studentController.findById(req, res)
 );
 
-loginRoutes.delete("/:id", validateToken, (req, res) =>
+loginRoutes.delete("/:id", validateToken, (req: Request, res: Response) =>
   studentController.delete(req, res)
 );
 
