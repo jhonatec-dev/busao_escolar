5a595154855ff1bf8788ed0e816adef74a1f43ec
Jhonatec <jhonsreis@gmail.com>
2023-11-01 22:12:12 -0300

    feat: frontend: redeploy

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -46,6 +46,7 @@ export default function Home() {
       }
     } catch (error: AxiosError | any) {
       if (error instanceof AxiosError) {
+        console.log('deu ruim geral')
         showMessage(error.response?.data.message, "error");
       } else showMessage(error.message, "error");
     }
